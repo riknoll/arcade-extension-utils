3d6bb11bd73b41358b8ef0b2a2534d819296a971
Richard Knoll <riknoll@microsoft.com>
2023-10-31 10:24:08 -0700

    add state factory

--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
-// add code here
+namespace __util {
+    export class StateFactory<U> {
+        protected stateStack: U[];
+
+        constructor(public create: () => U) {
+            this.stateStack = [this.create()];
+
+            game.addScenePushHandler(() => {
+                this.stateStack.push(this.create());
+            });
+
+            game.addScenePopHandler(() => {
+                this.stateStack.pop();
+                if (this.stateStack.length === 0) {
+                    this.stateStack.push(this.create());
+                }
+            });
+        }
+
+        state(): U {
+            return this.stateStack[this.stateStack.length - 1];
+        }
+    }
+
+    let registry: StateFactory<any>[];
+
+    export function getState<U>(factory: () => U): U {
+        if (!registry) registry = [];
+
+        for (const f of registry) {
+            if (f.create === factory) return f.state();
+        }
+
+        const newFactory = new StateFactory(factory);
+        registry.push(newFactory)
+
+        return newFactory.state();
+    }
+}
